2d1fc4fa7e35bdf33012296b511ac4b8088376fb
vinicius-r-silva <vinicius.r@usp.br>
2020-05-09 02:39:11 -0300

    commit interno

--- Atividade_2/prod_cons.c
+++ Atividade_2/prod_cons.c
@@ -20,6 +20,13 @@ typedef struct th_args
     void (*WaitFunction)();
 } th_args;
 
+typedef struct tist_t{
+
+    struct tist_t *next;
+    int item;
+
+}list_t;
+
 sem_t mutex, empty, full;
 
 pthread_mutex_t the_mutex;
@@ -30,31 +37,92 @@ int buffer[BUFFER_SIZE], count, itemCount, lastCount = 0;
 void *(*Produtor)(void *);
 void *(*Consumidor)(void *);
 
+list_t* begin = NULL;
+list_t* last = NULL;
+list_t* current = NULL;
+list_t* prev = NULL;
+
+void push(){
+    list_t *temp;
+
+    temp = (list_t*) malloc(sizeof(list_t));
+    temp->item = itemCount + 1;
+    temp->next = begin;
+    begin = temp;
+}
+
+void pop(){
+    list_t* temp;
+
+    temp = begin;
+    begin = begin->next;
+
+    free(temp);
+}
 
 void *Produtor_Sem(void *args){
     printf("Iniciando Produtor com semaforo");
     int item = 1;
 
+    list_t *temp;
+
     while (FOREVER) {
         sem_wait(&empty);
         sem_wait(&mutex);
 
+        // temp = begin;
+        // while (temp != NULL)
+        // {
+        //     printf("p: %d  ", temp->item);
+        //     temp = temp->next;
+        // }
+        // printf("\n");
+
+        if(itemCount == 0){
+            begin = (list_t*) malloc(sizeof(list_t));
+            begin->item = itemCount + 1;
+            begin->next = NULL;
+        }else{
+            push();
+        }
+        
         buffer[itemCount] = item;
         itemCount++;
+        printf("Produtor - item %d produzido\n", begin->item);
         printf("Produtor - item %d produzido\n", itemCount);
 
         sem_post(&mutex);
         sem_post(&full);
-        usleep(1);
+        usleep(1000);
     }
 }
 
 void *Consumidor_Sem(void *args){
     printf("Iniciando Consumidor com semaforo");
 
+    list_t *temp;
     while (FOREVER){
         sem_wait(&full);
         sem_wait(&mutex);
+
+        // // temp = begin;
+        // while (temp != NULL)
+        // {
+        //     printf("c: %d ", temp->item);
+        //     temp = temp->next;
+        // }
+        // printf("\n");
+
+        if(itemCount == 1){
+            free(begin);
+            begin = NULL;
+            current = NULL;
+            last = NULL;
+            printf("Consumidor - item 1 consumido\n");
+        }else{
+            printf("Consumidor - item %d consumido\n", begin->item);
+            pop();
+        }
 
         itemCount--;
         buffer[itemCount] = 0;
@@ -62,7 +130,7 @@ void *Consumidor_Sem(void *args){
 
         sem_post(&mutex);
         sem_post(&empty);
-        usleep(1);
+        usleep(1000);
     }
 }
 
@@ -78,25 +146,34 @@ void *Produtor_sleepWakeup(void *args)
             exit(EXIT_FAILURE);
         }
 
-        if (itemCount == BUFFER_SIZE)
-            printf("Produtor - Sem espaço para produzir, liberando buffer\n");
-        while (itemCount == BUFFER_SIZE){
-            pthread_cond_wait(&condp, &the_mutex);
-        }
-        
-
         pthread_mutex_lock(&the_mutex); /* protect buffer */
         printf("Produtor - Bloqueando Buffer\n");
 
+        if (itemCount >= BUFFER_SIZE){
+            printf("Produtor - Sem espaço para produzir, liberando buffer\n");
+            while (itemCount >= BUFFER_SIZE){
+                pthread_cond_wait(&condp, &the_mutex);
+            }
+        }
+       
+        if(itemCount == 0){
+            begin = (list_t*) malloc(sizeof(list_t));
+            begin->item = itemCount + 1;
+            begin->next = NULL;
+        }else{
+            push();
+        }
+
         buffer[itemCount] = 1;
-        printf("Produtor - item %d produzido\n", itemCount);
         lastCount = itemCount;
         itemCount++;
-
-        pthread_mutex_unlock(&the_mutex);
+        printf("Produtor - item %d produzido\n", begin->item);
+        printf("Produtor - item %d produzido\n", itemCount);
+        
         if (lastCount == 0){
             pthread_cond_signal(&condc); /* wake up consumer */
         }
+        pthread_mutex_unlock(&the_mutex);
 
         printf("Produtor - Liberando Buffer\n");
         usleep(10);
@@ -114,25 +191,40 @@ void *Consumidor_sleepWakeup(void *args)
             exit(EXIT_FAILURE);
         }
 
-      	
-        if (itemCount == 0)
-            printf("Consumidor - Sem nada para consumir, esperando produtor\n");
-      	while(itemCount == 0){
-            pthread_cond_wait(&condc, &the_mutex);            
-        }
-
         pthread_mutex_lock(&the_mutex); /* protect buffer */
         printf("Consumidor - Bloqueando Buffer\n");
+
+        if (itemCount <= 0){
+            printf("Consumidor - Sem nada para consumir, esperando produtor\n");
+            while(itemCount <= 0){
+                pthread_cond_wait(&condc, &the_mutex); 
+            }
+        }
+
+        if(itemCount == 1){
+            printf("Consumidor - item 1 consumido\n");
+            free(begin);
+            begin = NULL;
+            current = NULL;
+            last = NULL;
+        }else{
+            if(begin == NULL){
+                printf("Begin NULL");
+                getchar();
+            }
+            printf("Consumidor - item %d consumido\n", begin->item);
+            pop();
+        }
 
         lastCount = itemCount;
         itemCount--;
         buffer[itemCount] = 0;
-        printf("Consumidor - item %d consumido\n", itemCount);
+        printf("Consumidor - item %d consumido\n", itemCount+1);
 
-        pthread_mutex_unlock(&the_mutex);
         if (lastCount == BUFFER_SIZE){
             pthread_cond_signal(&condp); /* wake up consumer */
         }
+        pthread_mutex_unlock(&the_mutex);
 
         printf("Consumidor - Liberando Buffer\n");
         usleep(10);
@@ -176,12 +268,12 @@ int main(int argc, char *argv[]){
         exit(EXIT_FAILURE);
     }
 
+    pthread_join(ThHandle[0], NULL);
+    pthread_join(ThHandle[1], NULL);
     pthread_mutex_destroy(&the_mutex); /* Free up the_mutex */
     pthread_cond_destroy(&condc);      /* Free up consumer condition variable */
     pthread_cond_destroy(&condp);      /* Free up producer condition variable */
     // pthread_attr_destroy(&ThAttr);
-    pthread_join(ThHandle[0], NULL);
-    pthread_join(ThHandle[1], NULL);
 
     return (EXIT_SUCCESS);
 }
